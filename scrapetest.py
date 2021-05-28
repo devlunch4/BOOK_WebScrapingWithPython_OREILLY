bda92e5c4d4fca6d4ee6016ead2510f2b7095b54
woosung.kim <devlunch4@gmail.com>
2021-05-28 13:28:01 +0900

    ko_page 30. some test

--- scrapetest.py
+++ scrapetest.py
@@ -1,3 +1,12 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
 html = urlopen('http://pythonscraping.com/pages/page1.html')
-print(html.read())
+bs = BeautifulSoup(html.read(), 'html.parser')
+print('html>>>', bs.html)
+print()
+print('h1>>>', bs.h1)
+print()
+print('head>>>', bs.head)
+print()
+print('div>>>', bs.div)
