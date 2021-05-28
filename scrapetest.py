e671c2dcb67b18020d064a919dbe748e058cac99
woosung.kim <devlunch4@gmail.com>
2021-05-28 13:42:11 +0900

    ko_page 30. some test

--- scrapetest.py
+++ scrapetest.py
@@ -3,10 +3,4 @@ from bs4 import BeautifulSoup
 
 html = urlopen('http://pythonscraping.com/pages/page1.html')
 bs = BeautifulSoup(html.read(), 'html.parser')
-print('html>>>', bs.html)
-print()
-print('h1>>>', bs.h1)
-print()
-print('head>>>', bs.head)
-print()
-print('div>>>', bs.div)
+print(bs.html.body.h1)
