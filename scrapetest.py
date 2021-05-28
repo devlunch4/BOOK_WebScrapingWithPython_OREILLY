50c864725d4cf91534977bacc5ac82f8e416699b
woosung.kim <devlunch4@gmail.com>
2021-05-28 15:38:25 +0900

    ko_page 32. some test

--- scrapetest.py
+++ scrapetest.py
@@ -3,4 +3,13 @@ from bs4 import BeautifulSoup
 
 html = urlopen('http://pythonscraping.com/pages/page1.html')
 bs = BeautifulSoup(html.read(), 'html.parser')
-print(bs.html.body.h1)
+print(bs.html)
+print(bs.h1)
+
+# bs = BeautifulSoup(html.read(), 'lxml')
+# print('lxml', bs.html)
+# print('lxml', bs.h1)
+#
+# bs = BeautifulSoup(html.read(), 'html5lib')
+# print('html5lib', bs.html)
+# print('html5lib', bs.h1)
