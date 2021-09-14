37da3b427f4c7245849e5ac12bd930bb7b0d9165
devlunch4 <devlunch4@gmail.com>
2021-09-14 23:24:56 +0900

    20210914 KO ver Page 153. re-read. next do.

--- ch06.4_이메일.py
+++ ch06.4_이메일.py
@@ -7,7 +7,7 @@ msg['Subject'] = 'An Email Alert'
 msg['From'] = 'ryan@python.com'
 msg['To'] = 'webmaster@python.com'
 
-s = smtplib.SMTP('localhost')
+s = smtplib.SMTP('localhost') #이부분을 수정 필요
 s.send_message(msg)
 s.quit()
 
